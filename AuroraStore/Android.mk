0d951efe5ab1009354bc0b58a56c68a68893ce28
Vosky <41956+Vosky@users.noreply.github.com>
2022-12-13 18:20:38 +0100

    Update Android.mk

--- AuroraStore/Android.mk
+++ AuroraStore/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := AuroraStore
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 LOCAL_SRC_FILES := AuroraStore.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
